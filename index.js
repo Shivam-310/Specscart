4a2b3d477be304be3da8e0e0d969f98ad3d6a437
Shivam Chaudhary <shivamchaudhary310899@gmail.com>
2023-05-26 15:33:32 +0530

    final commit

--- index.js
+++ index.js
@@ -10,3 +10,55 @@ slider.addEventListener("input", (e) => {
   sliderLine.style.left = sliderValue;
   sliderIcon.style.left = sliderValue;
 });
+
+ //carousal
+
+ function expandElement(element) {
+  var container = document.querySelector('.container');
+  var imgBtns = document.querySelector('.carousal_img_text .carousla-img-btn');
+  var divBtns = document.querySelectorAll('.div1_btn, .div2_btn');
+
+  // Hide buttons
+  imgBtns.style.display = 'none';
+  divBtns.forEach(function(btn) {
+      btn.style.display = 'none';
+  });
+
+  if (element.classList.contains('expanded')) {
+      container.classList.remove('expanded');
+      element.classList.remove('expanded');
+      element.classList.add('collapsed');
+  } else {
+      container.classList.add('expanded');
+
+      var otherElements = document.querySelectorAll('.image1, .div1, .div2');
+      for (var i = 0; i < otherElements.length; i++) {
+          if (otherElements[i] !== element) {
+              otherElements[i].classList.add('collapsed');
+              otherElements[i].classList.remove('expanded');
+          }
+      }
+
+      element.classList.add('expanded');
+      element.classList.remove('collapsed');
+
+      // Show buttons
+      if (element.classList.contains('image1')) {
+          imgBtns.style.display = 'block';
+      } else {
+          var btnContainer = element.querySelector('.div1_btn, .div2_btn');
+          btnContainer.style.display = 'block';
+      }
+  }
+}
+
+// Hide buttons on page load
+window.addEventListener('DOMContentLoaded', function() {
+  var imgBtns = document.querySelector('.carousal_img_text .carousla-img-btn');
+  var divBtns = document.querySelectorAll('.div1_btn, .div2_btn');
+
+  imgBtns.style.display = 'none';
+  divBtns.forEach(function(btn) {
+      btn.style.display = 'none';
+  });
+});
